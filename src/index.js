7f10d360cf19703274c6fe008e4b2285e89962a4
MinecraftDawn <eric4902077@gmail.com>
2023-10-07 21:21:09 +0800

    Memento

--- src/index.js
+++ src/index.js
@@ -50,14 +50,14 @@ class Game extends React.Component {
                 squares: Array(9).fill(null),
 
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
     handleClick(i){
-        const history = this.state.history;
-        const current = history[history.length-1];
-
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[this.state.stepNumber];
         // Immutability
         const squares = current.squares.slice();
         if(calculateWinner(squares) || squares[i]){
@@ -69,18 +69,38 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext});
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length-1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move # ' + move:
+                'Go to game start';
+
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        })
         let status;
         if (winner){
             status = 'Winner: ' + winner;
         }else{
-            status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
 
 
@@ -92,17 +112,12 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
     }
 }
-
-// ========================================
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
 
 function calculateWinner(squares){
     const lines = [
@@ -127,3 +142,9 @@ function calculateWinner(squares){
     }
     return null;
 }
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
+
