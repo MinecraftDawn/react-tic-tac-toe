ed99065d3a4af05a2b75a3c435949bb9ece464e8
MinecraftDawn <eric4902077@gmail.com>
2023-10-08 10:19:17 +0800

    get data from server send data to server

--- src/index.js
+++ src/index.js
@@ -4,41 +4,40 @@ import './index.css';
 
 function Square(props) {
     return (
-            <button className="square" disabled={!props.enable}
-                    onClick={props.onClick}>
-                {props.value}
-            </button>
+        <button className="square" disabled={!props.enable}
+                onClick={props.onClick}>
+            {props.value}
+        </button>
     )
 }
 
-class Board extends React.Component {
+function Board(props) {
 
-    renderSquare(i) {
-        return <Square value={this.props.squares[i]} enable={this.props.enable}
-                onClick={()=> this.props.onClick(i) } />;
+    function renderSquare(i) {
+        return <Square value={props.squares[i]} enable={props.enable}
+                       onClick={() => props.onClick(i)}/>;
     }
 
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
-        );
-    }
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+
 }
 
 class Game extends React.Component {
@@ -48,7 +47,7 @@ class Game extends React.Component {
         this.inputRef = React.createRef();
 
         this.state = {
-            history : [{
+            history: [{
                 squares: Array(9).fill(null),
 
             }],
@@ -60,13 +59,13 @@ class Game extends React.Component {
 
     }
 
-    handleClick(i){
+    handleClick(i) {
         const history = this.state.history;
-        const current = history[history.length-1];
+        const current = history[history.length - 1];
 
         // Immutability
         const squares = current.squares.slice();
-        if(calculateWinner(squares) || squares[i]){
+        if (calculateWinner(squares) || squares[i]) {
             return;
         }
 
@@ -75,41 +74,54 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
-            xIsNext: !this.state.xIsNext});
+            xIsNext: !this.state.xIsNext
+        });
+
+        fetch(`https://localhost:7138/TicTacToe/game?player=${this.state.name}&number=${i}`,
+            {method: 'PATCH'});
 
     }
 
-    updateName(event){
+    updateName(event) {
         this.setState({
-            name:event.target.value,
+            name: this.inputRef.current.value
         });
+
+        fetch('https://localhost:7138/TicTacToe/game', {method: 'POST'});
+
+        fetch('https://localhost:7138/TicTacToe/game?player='+this.inputRef.current.value,
+            {method:'GET'}).then((resp) =>{
+                return resp.json()
+        }).then(data =>{
+            this.setState(data);
+        })
 
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length-1];
+        const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
 
         let status;
-        if (winner){
+        if (winner) {
             status = 'Winner: ' + winner;
-        }else{
+        } else {
             status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
         }
 
 
         return (
             <div className="game">
-                <div className="game-board" >
-                    <Board squares = {current.squares} enable={this.state.name!==""}
-                    onClick={(i)=>this.handleClick(i)}/>
+                <div className="game-board">
+                    <Board squares={current.squares} enable={this.state.name !== ""}
+                           onClick={(i) => this.handleClick(i)}/>
                 </div>
                 <div className="game-info">
-                    <input ref={this.inputRef} disabled={this.state.name!==""}></input>
-                    <button disabled={this.state.name!==""}
-                            onClick={(event)=>{this.setState({'name':this.inputRef.current.value})}}>
+                    <input ref={this.inputRef} disabled={this.state.name !== ""}></input>
+                    <button disabled={this.state.name !== ""}
+                            onClick={this.updateName}>
                         確認名字
                     </button>
                     <div>{status}</div>
@@ -125,26 +137,25 @@ class Game extends React.Component {
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(<Game/>);
 
-function calculateWinner(squares){
+function calculateWinner(squares) {
     const lines = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ];
 
     for (let i = 0; i < lines.length; i++) {
         let line = lines[i];
-        console.log("line"+line);
-        if(squares[line[0]] === squares[line[1]] &&
+        if (squares[line[0]] === squares[line[1]] &&
             squares[line[1]] === squares[line[2]] &&
-            squares[line[0]]){
+            squares[line[0]]) {
             return squares[line[0]];
         }
     }
